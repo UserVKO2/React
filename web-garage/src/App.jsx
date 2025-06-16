0dbfdd01a51cd690125d66f13ea2a835483792e7
UserVKO2 <medet.uk@gmail.com>
2025-06-17 03:15:15 +0600

    My ferst commit

---
+++ web-garage/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-6xl'>App component</h1>
+    </>
+  )
+}
+
+export default App
